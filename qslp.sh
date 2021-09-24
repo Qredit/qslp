584a15afcd6d6e673278135ef4bd9991d70cd06c
Nayiem Willems <37852059+NayiemWillems@users.noreply.github.com>
2021-09-24 15:57:55 +0200

    Update qslp.sh

--- qslp.sh
+++ qslp.sh
@@ -55,38 +55,87 @@ three(){
 four(){
         echo "Stopping QSLP"
         pm2 stop qslpParser.js
+        pm2 stop qslpApi.js
+        echo "QSLP has been succesfully stopped."
+	sleep 1
+	echo "Starting QSLP"
+        pm2 start qslpParser.js
+	sleep 1
+	pm2 start qslpApi.js
+        echo "QSLP has been succesfully started."
+        pause
+} 
+
+five(){
+        echo "Stopping QSLP"
+        pm2 stop qslpParser.js
         sleep 1
         pm2 stop qslpApi.js
         echo "QSLP has been succesfully stopped."
         pause
 } 
 
-five(){
+six(){
         echo "Starting PM2 Monitor"
         pm2 monit
         pause
 } 
 
-six(){
+seven(){
         echo "PM2 Status"
         pm2 status   
+        pause
+} 
+
+eight(){
+	echo "Stopping all QSLP Processes"
+	pm2 stop qslpParser.js
+        pm2 stop qslpApi.js
+	echo "Pulling updates"
+	sleep 1
+	git pull
+	echo "Installing Prerequisites"
+	sleep 1
+	sudo apt-get -y install curl dirmngr apt-transport-https lsb-release ca-certificates
+	sudo apt-get -y install mongodb
+	sudo apt-get -y install redis-server
+	echo "Resetting Config"
+	sleep 1
+	cp qslp.ini.example qslp.ini
+	echo "Adding Port 8001 to UFW"
+	sleep 1
+	sudo ufw allow 8001
+	echo "Installing QSLP"
+	sleep 1
+	yarn install
+	echo "Starting QSLP"
+	sleep 1
+        pm2 start qslpParser.js
+	sleep 1
+	pm2 start qslpApi.js
+        echo "QSLP has been succesfully installed and launched."
         pause
 } 
 # function to display menus
 show_menus() {
 	clear
-	echo "~~~~~~~~~~~~~~~~~~~~~"
-	echo " QSLP Sidechain v1.0 "
-        echo "~~~~~~~~~~~~~~~~~~~~~"
-	echo "  M A I N - M E N U  "
-	echo "~~~~~~~~~~~~~~~~~~~~~"
+	echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+	echo "      Qredit Side Ledger Protocol      "
+	echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+	echo "      QSLP Sidechain Manager v1.1      "
+        echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+	echo "           M A I N - M E N U           "
+	echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 	echo "1. Install QSLP"
 	echo "2. Update QSLP"
-	echo "3. Start QSLP Node"
-	echo "4. Stop QSLP Node"
-	echo "5. PM2 Monitor"
-	echo "6. View Status"
+	echo "3. Start QSLP"
+	echo "4. Restart QSLP"
+	echo "5. Stop QSLP"
+	echo "6. PM2 Monitor"
+	echo "7. View Status"
+	echo "8. Reinstall, Reset Config & Start QSLP"
 	echo "9. Exit"
+	echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 }
 # read input from the keyboard and take a action
 # invoke the one() when the user select 1 from the menu option.
@@ -102,6 +151,8 @@ read_options(){
                 4) four ;;
                 5) five ;;
                 6) six ;;
+		7) seven ;;
+		8) eight ;;
 		9) exit 0;;
 		*) echo -e "${RED}Error...${STD}" && sleep 2
 	esac
